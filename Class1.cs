35082304d5bc104e0d303067ab136be5ec7b6ef5
generalpranay <pranayhasmukh.bhalsod@edu.sait.ca>
2024-06-12 18:34:41 -0600

    second push

---
+++ Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace assignment_1
+{
+    public class Appliance
+    {
+        public string ItemNumber { get; set; }
+        public string Brand { get; set; }
+        public int Quantity { get; set; }
+        public int Wattage { get; set; }
+        public string Color { get; set; }
+        public double Price { get; set; }
+
+        public Appliance(string itemNumber, string brand, int quantity, int wattage, string color, double price)
+        {
+            ItemNumber = itemNumber;
+            Brand = brand;
+            Quantity = quantity;
+            Wattage = wattage;
+            Color = color;
+            Price = price;
+        }
+    }
+    class Refrigerator : Appliance
+    {
+        public int NumberOfDoors { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public Refrigerator(string itemNumber, string brand, int quantity, int wattage, string color, double price,int numberofdoors,int height,int width) : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            NumberOfDoors = numberofdoors;
+            Height = height;
+            Width = width;
+        }
+    }
+    class Vacuum:Appliance
+    {
+        public string Grade { get; set; }
+        public int BatteryVoltage { get; set; }
+
+        public Vacuum(string itemNumber, string brand, int quantity, int wattage, string color, double price,string grade,int batteryvoltage) : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            Grade = grade;
+            BatteryVoltage = batteryvoltage;
+        }
+
+    }
+    class Microwave : Appliance
+    {
+        public double Capacity { get; set; }
+        public string RoomType { get; set; }
+
+        public Microwave(string itemNumber, string brand, int quantity, int wattage, string color, double price, double capacity, string roomtype) : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            Capacity = capacity;
+            RoomType = roomtype;
+        }
+
+    }
+    class Dishwasher : Appliance
+    {
+        public string Feature { get; set; }
+        public string SoundRating { get; set; }
+
+        public Dishwasher(string itemNumber, string brand, int quantity, int wattage, string color, double price, string feature, string soundrating) : base(itemNumber, brand, quantity, wattage, color, price)
+        {
+            Feature = feature;
+            SoundRating = soundrating;
+        }
+    }
+}
